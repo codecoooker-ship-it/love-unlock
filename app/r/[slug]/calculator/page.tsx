e525ea6cb7a6fa5e85389d9995e38d5cff417465
Zihad <codecoooker@gmail.com>
2026-02-16 15:41:46 +0600

    fix love calculator component

--- app/r/[slug]/calculator/page.tsx
+++ app/r/[slug]/calculator/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoveCalculatorClient from "@/components/LoveCalculator";
 import LoveCalculator from "@/components/LoveCalculator"; // (যদি component এ রাখো)
 export default function Page() {
   return <LoveCalculator />;
